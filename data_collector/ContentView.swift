cac6ed98e7d464ba6400ca9ba443818162dfa0f7
Naman Arora <59131920+ArorAnam@users.noreply.github.com>
2023-10-30 14:43:54 +0000

    chnage on screen view

--- data_collector/ContentView.swift
+++ data_collector/ContentView.swift
@@ -13,7 +13,7 @@ import CoreLocationUI
 import CoreMotion
 
 extension CLLocationCoordinate2D {
-    static let parking = CLLocationCoordinate2D(latitude: 53.489778, longitude: -2.214486)
+    static let parking = CLLocationCoordinate2D(latitude: 53.34156, longitude: -6.25321)
 }
 
 struct ContentView: View {
@@ -27,49 +27,78 @@ struct ContentView: View {
     @ObservedObject var locationManager = LocationManager()
 
     var body: some View {
-        Map(selection: $selectedTag){
-            Marker("Start Point", coordinate: .parking)
-                .tint(.blue)
-                .tag(1)
-        }
-        .mapStyle(.standard(elevation: .realistic))
-
-        VStack{
-            Text("\(String(format: "%.0f", locationManager.degrees))°".uppercased())
-
-            if let myLocation = locationManager.location{
-                Text("Latitude: \(myLocation.latitude.formatted(.number.precision(.fractionLength(3)))), Longitude: \(myLocation.longitude.formatted(.number.precision(.fractionLength(3))))")
-            } else {
-                LocationButton{
-                    locationManager.requestLocation()
+            ZStack{
+                Map(selection: $selectedTag){
+                    Marker("Start Point", coordinate: .parking)
+                        .tint(.blue)
+                        .tag(1)
                 }
-                .labelStyle(.iconOnly)
-                .cornerRadius(20)
-            }
+                .mapStyle(.standard(elevation: .realistic))
+                .safeAreaInset(edge: .bottom) {
+                    
+                    VStack{
+                        Text("Magnetometer")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        Text("Direction: \(String(format: "%.0f", locationManager.degrees))°")
+                        Text("Accelerometer")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        
+                        if let accelerometerData = motionManager.accelerometerData {
+                            Text("X: \(accelerometerData.acceleration.x), Y: \(accelerometerData.acceleration.y), Z: \(accelerometerData.acceleration.z)")
+                        }
 
-            if let altitude = altitudeManager.location?.altitude {
-                Text("\(String(format: "%.3f", altitude))m")
-            }
+                        Text("Gyroscope")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        
+                        if let gyroData = motionManager.gyroData {
+                            Text("X: \(gyroData.rotationRate.x), Y: \(gyroData.rotationRate.y), Z: \(gyroData.rotationRate.z)")
+                        }
 
-            if let accelerometerData = motionManager.accelerometerData {
-                Text("\(accelerometerData)")
-            }
+                        if let myLocation = locationManager.location, let altitude = altitudeManager.location{
+                            Text("Barometer")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                            Text("Altitude: \(String(format: "%.3f", altitude.altitude))m")
+                            Text("GPS")
+                                .font(.title3)
+                                .fontWeight(.semibold)
 
-            if let gyroData = motionManager.gyroData {
-                Text("\(gyroData)")
-            }
+                            Text("Latitude: \(myLocation.latitude.formatted(.number.precision(.fractionLength(3)))), Longitude: \(myLocation.longitude.formatted(.number.precision(.fractionLength(3))))")
+                        } else {
+                            LocationButton{
+                                locationManager.requestLocation()
+                            }
+                            .labelStyle(.iconOnly)
+                            .cornerRadius(20)
+                        }
+                    }
+                    .onAppear(){
+                        altitudeManager.requestWhenInUseAuthorization()
+                        altitudeManager.startUpdatingLocation()
+                        motionManager.startGyroUpdates()
+                        motionManager.startAccelerometerUpdates()
 
-        }
-        .onAppear(){
-            altitudeManager.requestWhenInUseAuthorization()
-            altitudeManager.startUpdatingLocation()
-            motionManager.startGyroUpdates()
-            motionManager.startAccelerometerUpdates()
-        }
+                    }
+                    .frame(width: 350, height: 300)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(10)
+                }
+            }
+        
+//        if let data = altitudeManager.location, let data_2 = locationManager.location{
+//
+//                let _ = print("\(String(describing: data_2.latitude )), \(String(describing: data_2.longitude)), \(locationManager.degrees), \(data.altitude), \(getTime())")
+//
+//        }
+
 
         if let altitude_data = altitudeManager.location, let accelerator_Data = motionManager.accelerometerData, let gyro_Data = motionManager.gyroData{
             let _ = print("\(String(describing: locationManager.degrees)), \(String(describing: accelerator_Data.acceleration.x)), \(String(describing: accelerator_Data.acceleration.y)), \(String(describing: accelerator_Data.acceleration.z)), \(String(describing: gyro_Data.rotationRate.x)), \(String(describing: gyro_Data.rotationRate.y)), \(String(describing: gyro_Data.rotationRate.z)), \(String(describing: altitude_data.altitude)), \(String(describing: getTime()))")
         }
+        
     }
 
 }
